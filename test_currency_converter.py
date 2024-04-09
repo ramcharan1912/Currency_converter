29661e6b5a4b24f3d1641a8f6302cd3c6fd13010
Ram charan <211119@iiitt.ac.in>
2024-04-09 19:34:13 +0530

    testing blank inputs

--- test_currency_converter.py
+++ test_currency_converter.py
@@ -3,9 +3,13 @@ from currency_converter import dollar_to_rupee
 
 class TestCurrencyConversion(unittest.TestCase):
 
-     def test_dollar_to_rupee(self):
+      def test_dollar_to_rupee(self):
         self.assertAlmostEqual(dollar_to_rupee(10), 740)
         self.assertAlmostEqual(dollar_to_rupee(20), 1480)
+
+      def test_blank_input_dollar_to_rupee(self):
+        with self.assertRaises(ValueError):
+            dollar_to_rupee('')  
    
 
 if __name__ == '__main__':
