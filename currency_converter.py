30d98dcb4a580e3634b16cbc7f16a249285fc049
Ram charan <211119@iiitt.ac.in>
2024-04-09 19:40:42 +0530

    testing inputs other than numbers

--- currency_converter.py
+++ currency_converter.py
@@ -2,6 +2,9 @@ def dollar_to_rupee(amount):
 
     if amount == '':
         raise ValueError("Input cannot be blank")
+
+    if not isinstance(amount, (int, float)):
+        raise ValueError("Input must be numeric")	
     
     return amount * 74
 
